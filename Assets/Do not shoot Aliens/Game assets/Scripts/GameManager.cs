a0226f1f925b58f394d5d100a24601087d95776c
Rotciv Ocnarb <rotciv_ocnarb@hotmail.com>
2019-07-16 20:53:51 -0300

    Initial Commit

---
+++ Assets/Do not shoot Aliens/Game assets/Scripts/GameManager.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Firebase.Analytics;
+using Firebase.Messaging;
+using Firebase.InstanceId;
+
+public class GameManager : MonoBehaviour {
+	
+	public Animator startPanel;
+	public Animator gamePanel;
+	public Animator gameOverPanel;
+	public Animator startOverlay;
+	public Animator tutorial;
+	public Animator moveCamera;
+	public Animator transition;
+	
+	public Text bestText;
+	public Text scoreText;
+	
+	[HideInInspector]
+	public bool gameStarted;
+    public string gameId;
+    public int totalRedAliens = 0;
+    public int aliensKilled = 0;
+    public float timePlayed = 0;
+	
+	Spawner spawner;
+	PlayerController player;
+	
+	void Start(){
+        Debug.Log("Start Game Manager");
+        spawner = GameObject.FindObjectOfType<Spawner>();
+		player = GameObject.FindObjectOfType<PlayerController>();
+
+        FirebaseMessaging.TokenReceived += OnTokenReceived;
+        FirebaseMessaging.MessageReceived += OnMessageReceived;
+
+        FirebaseMessaging.TokenRegistrationOnInitEnabled = true;
+
+        Debug.Log("Begin searching token");
+        new System.Threading.Tasks.Task( async () => {
+            string token = await FirebaseInstanceId.DefaultInstance.GetTokenAsync();
+            Debug.Log(token);
+        }).Start();
+        
+    }
+
+    public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+    {
+        UnityEngine.Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+    {
+        UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+
+    void Update(){
+        if (gameStarted)
+        {
+            timePlayed += Time.deltaTime;
+        }
+
+		if(Input.GetMouseButtonDown(0)){
+			if(!gameStarted){
+				StartGame();
+			}
+			else if(!gamePanel.gameObject.activeSelf){
+				StartCoroutine(RestartGame());
+			}
+		}
+	}
+
+    string GenerateRandomString(int charAmount)
+    {
+        const string glyphs = "abcdefghijklmnopqrstuvwxyz0123456789"; //add the characters you want
+        string ret = "";
+        for (int i = 0; i < charAmount; i++)
+        {
+            ret += glyphs[Random.Range(0, glyphs.Length)];
+        }
+        return ret;
+    }
+	
+	void StartGame(){
+		gameStarted = true;
+        totalRedAliens = 0;
+        aliensKilled = 0;
+        timePlayed = 0;
+
+        moveCamera.SetTrigger("MoveCamera");
+		
+		startPanel.SetTrigger("Fade");
+		startOverlay.SetTrigger("Fade");
+		
+		tutorial.SetBool("Invisible", false);
+		
+		gamePanel.SetBool("Visible", true);
+		
+		spawner.StartSpawning();
+
+        //Firebase analytics
+        gameId = GenerateRandomString(10);
+        FirebaseAnalytics.LogEvent("game_start", new Parameter[] {
+               new Parameter("gameId", gameId)
+        });
+	}
+    
+	public void GameOver(){
+		if(!gamePanel.gameObject.activeSelf)
+			return;
+		
+		Target target = GameObject.FindObjectOfType<Target>();
+		int score = target.GetScore();
+		
+		if(score > PlayerPrefs.GetInt("Best"))
+			PlayerPrefs.SetInt("Best", score);
+		
+		bestText.text = PlayerPrefs.GetInt("Best") + "";
+		scoreText.text = score + "";
+		
+		gamePanel.gameObject.SetActive(false);
+		gameOverPanel.SetTrigger("Game over");
+		
+		player.Die();
+	}
+	
+	IEnumerator RestartGame(){
+		transition.SetTrigger("Transition");
+		
+		yield return new WaitForSeconds(0.5f);
+		
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
---
+++ Assets/Do not shoot Aliens/Game assets/Scripts/Target.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Analytics;
+
+public class Target : MonoBehaviour {
+	
+	public GameObject carveNavmesh;
+	public Animator anim;
+	public float leaveDistance;
+    public GameManager gameManager;
+	
+	public Animator tutorial;
+	
+	public Text scoreText;
+	int score;
+	
+	MoveArea area;
+	Transform player;
+
+    private float timeOnCheckpoint;
+	
+	void Start(){
+		area = GameObject.FindObjectOfType<MoveArea>();
+		
+		carveNavmesh.SetActive(false);
+		scoreText.gameObject.SetActive(false);
+	}
+	
+	void Update(){
+		if(anim.GetBool("Open") && Vector3.Distance(transform.position, player.position) > leaveDistance){
+			anim.SetBool("Open", false);
+		
+			StartCoroutine(LeftCircle());
+
+            //Firebase Analytics
+            FirebaseAnalytics.LogEvent("checkpoint_exit", new Parameter[] {
+                new Parameter("game_id", gameManager.gameId),
+                new Parameter("points", score),
+                new Parameter("red_aliens", gameManager.totalRedAliens),
+                new Parameter("time_on_checkpoint", timeOnCheckpoint)
+            }) ;
+		}
+        else if (anim.GetBool("Open"))
+        {
+            timeOnCheckpoint += Time.deltaTime;
+        }
+	}
+	
+	public void OnTriggerEnter(Collider other){
+		if(!other.gameObject.CompareTag("Player"))
+			return;
+
+        //Firebase Analytics
+        timeOnCheckpoint = 0;
+        FirebaseAnalytics.LogEvent("checkpoint_enter", new Parameter[] {
+               new Parameter("gameId", gameManager.gameId),
+               new Parameter("points", score),
+               new Parameter("red_aliens", gameManager.totalRedAliens)
+        }) ;
+
+		carveNavmesh.SetActive(true);
+		anim.SetBool("Open", true);
+		
+		player = other.gameObject.transform;
+		player.GetComponent<PlayerController>().SwitchSafeState(true);
+		
+		AddPoints();
+	}
+	
+	void AddPoints(){
+		if(!scoreText.gameObject.activeSelf)
+			scoreText.gameObject.SetActive(true);
+		
+		score++;
+		scoreText.text = "" + score;
+	}
+	
+	public int GetScore(){
+		return score;
+	}
+	
+	IEnumerator LeftCircle(){		
+		if(!tutorial.GetBool("Invisible"))
+			tutorial.SetBool("Invisible", true);
+	
+		yield return new WaitForSeconds(0.5f);
+		
+		carveNavmesh.SetActive(false);
+		transform.position = area.RandomPosition();
+		player.GetComponent<PlayerController>().SwitchSafeState(false);
+	}
+}
